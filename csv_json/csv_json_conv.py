54bf0f839d8fff203c1cf96812b96af915449fef
Rerice <79257429+Rerice@users.noreply.github.com>
2021-06-15 21:47:13 +0300

    version 1.1.1

--- csv_json/csv_json_conv.py
+++ csv_json/csv_json_conv.py
@@ -2,27 +2,22 @@ import json
 import csv
 import os
 
+def flatten_json (value, name = '', out = {}):
+    if isinstance(value, dict):
+        for item in value:
+            flatten_json(value[item], name + item + '_')
+    elif isinstance(value, list):
+        i = 0
+        for item in value:                
+            flatten_json(item, name + str(i) + '_')
+            i += 1
+    else: out[name[:-1]] = value
+    return out
+
 def json_to_csv(file_json):
 
     with open (file_json, "r") as f:
 
-        def flatten_json(data):
-            out = {}
-
-            def flatten(value, name =''):
-                if isinstance(value, dict):
-                    for item in value:
-                        flatten(value[item], name + item + '_')
-                elif isinstance(value, list):
-                    i = 0
-                    for item in value:                
-                        flatten(item, name + str(i) + '_')
-                        i += 1
-                else: out[name[:-1]] = value
-            
-            flatten(data)
-            return out
-    
         data = json.load(f)
         data = flatten_json(data)
         headers = data.keys()
@@ -34,7 +29,6 @@ def json_to_csv(file_json):
         writer.writerow(data)
         return os.path.abspath(os.path.splitext(file_json)[0]+".csv")
 
-
 def csv_to_json(file_csv):
 
     with open (file_csv, "r") as f:
@@ -43,15 +37,12 @@ def csv_to_json(file_csv):
         reader = csv.reader(f)
         arr_of_keys = reader.__next__()
         for row in reader:
-            Dict_of_data = {}
-            i = 0
+            dict_of_data = {}
             if not row:
                 continue
-            for key in arr_of_keys:
-                if i <= len(row):
-                    Dict_of_data[key] = row[i]
-                    i += 1
-            data.append(Dict_of_data)
+            for i, key in enumerate(arr_of_keys):
+                dict_of_data[key] = row[i]
+            data.append(dict_of_data)
 
     with open (os.path.splitext(file_csv)[0]+".json", "w") as f:
 
--- tests/test_csv_json.py
+++ tests/test_csv_json.py
@@ -9,8 +9,8 @@ def abs_path (file):
 class test_Csv_Json_Conv (unittest.TestCase):
        
     def test_json_2_csv (self):
-        f1_list = list()
-        f2_list = list()
+        f1_list = []
+        f2_list = []
         f1 = open (abs_path("file1_example.csv"), "r")
         json_to_csv(abs_path("file1.json"))
         f2 = open (abs_path("file1.csv"), "r")
@@ -18,11 +18,13 @@ class test_Csv_Json_Conv (unittest.TestCase):
             f1_list.append(rows1)
         for rows2 in f2:
             f2_list.append(rows2)
+        f1.close()
+        f2.close()
         self.assertListEqual(f1_list, f2_list)
 
     def test_csv_2_json (self):
-        f1_list = list()
-        f2_list = list()
+        f1_list = []
+        f2_list = []
         f1 = open (abs_path("file2_example.json"), "r")
         csv_to_json(abs_path("file2.csv"))
         f2 = open (abs_path("file2.json"), "r")
@@ -30,6 +32,8 @@ class test_Csv_Json_Conv (unittest.TestCase):
             f1_list.append(rows1)
         for rows2 in f2:
             f2_list.append(rows2)
+        f1.close()
+        f2.close()
         self.assertListEqual(f1_list, f2_list)
 
 if __name__ == "__name__":
